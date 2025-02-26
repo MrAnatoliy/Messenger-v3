e5b1f7bc873dcef39dc678e701ca69e5e613f0d0
AnatoliyR <95148318+MrAnatoliy@users.noreply.github.com>
2025-02-26 12:04:43 +0300

    Add CORS middleware and lifespan for mongodb session managmement

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,28 @@
+from contextlib import asynccontextmanager
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from pydantic import BaseModel
-
+from src.models.databases.chat_history import lifespan
 from src.api.api import secure_api_router
 from src.api.api import auth_api_router
 
-app = FastAPI()
+
+app = FastAPI(lifespan = lifespan)
 app.include_router(auth_api_router)
 app.include_router(secure_api_router)
+
+origins = [
+    "http://localhost:5173",  # Replace with your client's URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # Allows only the specified origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
