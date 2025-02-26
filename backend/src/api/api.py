c372a8a1a0555cef8ea10a7660016e24692abc7c
AnatoliyR <95148318+MrAnatoliy@users.noreply.github.com>
2025-02-26 12:05:25 +0300

    remove testing "hello" endpoint and renamet login to auth because login and auth merge in single file

--- backend/src/api/api.py
+++ backend/src/api/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 
+from src.api.endpoints.auth import auth
 from src.api.dependencies import get_current_active_user
 from src.api.endpoints.auth import (
-    login,
-    register,
+    auth
 )
 
 from src.api.endpoints.user_chat_db import (
@@ -15,16 +15,11 @@ from src.api.endpoints.chat import (
     message
 )
 
-from src.api.endpoints import (
-    hello
-)
-
 # -------------------------------- Auth routes ---------------------------------------
 auth_api_router = APIRouter(
     prefix="/api/v1",
 )
-auth_api_router.include_router(login.router, prefix="/login", tags=["login"])
-auth_api_router.include_router(register.router, prefix="/register", tags=["register"])
+auth_api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 auth_api_router.include_router(message.router, prefix="/message", tags=["message"])
 #-------------------------------------------------------------------------------------
 
@@ -34,7 +29,6 @@ secure_api_router = APIRouter(
     prefix="/api/v1/secure",
     dependencies = [Depends(get_current_active_user)]
 )
-secure_api_router.include_router(hello.router, prefix="/hello", tags=["hello"])
 secure_api_router.include_router(user_crud.router, prefix="/user", tags=["user"])
 secure_api_router.include_router(chat_crud.router, prefix="/chatroom", tags=["chatroom"])
 #-------------------------------------------------------------------------------------
